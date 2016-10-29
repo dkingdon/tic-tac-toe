68c019a12bb1e3be84fd7fd2f0b2602debe575ff
dkingdon <daniel.kingdon@gmail.com>
2016-10-29 00:01:26 -0700

    Changed card changing conditionals to switch statement and added code that changes the id of each square after selected preventing multiple turns on one square

--- js/app.js
+++ js/app.js
@@ -4,46 +4,65 @@ $(document).ready(function() {
   // goes inside this function
   var testVar
   var test = 'O'
+
+
   $('.col-md-4').click(function() {
-        /* Grabs unique square id and places it in the var testVar*/
-        testVar = (this.id);
-        /* Contitional statement that determines whos turn it is X or O*/
+        /* Grabs unique square id and places it in the var testVar */
+    testVar = (this.id);
+    /* Decideds if the square is clickable via an id check */
+    if (testVar !== 'notAvail') {
+        /* Contitional statement that determines whos turn it is X or O */
         if (test === 'O'){
             test = 'X';
         }
         else {
             test = 'O';
         }
-        /* Change square text value*/
-               if (testVar === 'topLeft') {
-                 $('#topLeft').text(test);
-               }
-               else if (testVar === 'topMiddle') {
-                 $('#topMiddle').text(test);
-               }
-               else if (testVar === 'topRight') {
-                 $('#topRight').text(test);
-               }
-               else if (testVar === 'midLeft') {
-                 $('#midLeft').text(test);
-               }
-               else if (testVar === 'midMiddle') {
-                 $('#midMiddle').text(test);
-               }
-               else if (testVar === 'midRight') {
-                 $('#midRight').text(test);
-               }
-               else if (testVar === 'botLeft') {
-                 $('#botLeft').text(test);
-               }
-               else if (testVar === 'botMiddle') {
-                 $('#botMiddle').text(test);
-               }
-               else if (testVar === 'botRight') {
-                 $('#botRight').text(test);
-               }
+        /* Change square text value */
+        switch (testVar) {
+            case 'topLeft':
+                $('#topLeft').text(test);
+                $('#topLeft').prop('id', 'notAvail')
+                break;
+            case 'topMiddle':
+                $('#topMiddle').text(test);
+                $('#topMiddle').prop('id', 'notAvail');
+                break;
+            case 'topRight':
+                $('#topRight').text(test);
+                $('#topRight').prop('id', 'notAvail');
+                break;
+            case 'midLeft':
+                $('#midLeft').text(test);
+                $('#midLeft').prop('id', 'notAvail');
+                break;
+            case 'midMiddle':
+                $('#midMiddle').text(test);
+                $('#midMiddle').prop('id', 'notAvail');
+                break;
+            case 'midRight':
+                $('#midRight').text(test);
+                $('#midRight').prop('id', 'notAvail');
+                break;
+            case 'botLeft':
+                $('#botLeft').text(test);
+                $('#botLeft').prop('id', 'notAvail');
+                break;
+            case 'botMiddle':
+                $('#botMiddle').text(test);
+                $('#botMiddle').prop('id', 'notAvail');
+                break;
+            case 'botRight':
+                $('#botRight').text(test);
+                $('#botRight').prop('id', 'notAvail');
+                break;
+            default:
+                break;
+            }
+    }
  });
 });
+
 
 // alert('linked!');
 
